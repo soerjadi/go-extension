335ca7b97fe864dc41b81cc45b5801c5b93bfa95
surya <soerja.610@gmail.com>
2019-11-27 16:06:47 +0700

    add command generate usecase

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,14 @@
 import { ExtensionContext } from 'vscode';
 
 import * as repository from './repository';
+import * as usecase from './usecase';
 import * as model from './model';
 import * as test from './test';
 
 export function activate(context: ExtensionContext) {
 
     repository.setup(context);
+    usecase.setup(context);
     model.setup(context);
     test.setup(context);
 
